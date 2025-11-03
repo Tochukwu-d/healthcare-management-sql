14854d05069b9c3e4eb9e865789a6ccb1eaace7c
IkwelleTochukwu <tochiantho99@gmail.com>
2025-11-03 07:29:27 +0100

    main.sql

--- main.sql
+++ main.sql
@@ -1,2 +1,39 @@
-use healthcaredata_db;
-show tables;
+show tables from healthcaredata_db;
+
+# to view the table structure and information
+describe appointment;
+describe patients;
+describe department;
+
+# check number of records in tables
+SELECT 'appointment' AS table_name, COUNT(*) AS total_records FROM appointment
+UNION
+SELECT 'patients', COUNT(*) FROM patients
+UNION
+SELECT 'bed', COUNT(*) FROM bed
+UNION
+SELECT 'bedrecords', COUNT(*) FROM bedrecords
+UNION
+SELECT 'department', COUNT(*) FROM department
+UNION
+SELECT 'doctors', COUNT(*) FROM doctors
+UNION
+SELECT 'helpers', COUNT(*) FROM helpers
+UNION
+SELECT 'medicalrecords', COUNT(*) FROM medicalrecords
+UNION
+SELECT 'nurse', COUNT(*) FROM nurse
+UNION
+SELECT 'room', COUNT(*) FROM room
+UNION
+SELECT 'roomrecords', COUNT(*) FROM roomrecords
+UNION
+SELECT 'staffshift', COUNT(*) FROM staffshift
+UNION
+SELECT 'surgeryrecords', COUNT(*) FROM surgeryrecords
+UNION
+SELECT 'wards', COUNT(*) FROM wards;
+
+
+
+ 
